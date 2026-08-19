2a3c99de487086fce8ff0e02976f0fbd2359f071
IbrohimjonCoder <muhammadjonovichibrohimjongmail.com>
2026-08-19 14:40:03 +0500

    students.json ishlamaybdi

--- Mustaqil/misol4.py
+++ Mustaqil/misol4.py
@@ -1,5 +1,16 @@
 import json
 
-with open("students.json") as file:
-        data = json.load(file)
-        
+with open("students.json", "r", encoding="utf-8") as file:
+    students = json.load(file)
+
+
+best = max(students, key=lambda x: x["grade"])
+
+worst = None
+
+average = None
+
+# 5. Natijani chiqarish
+print(f"Eng yaxshi talaba: {best['name']} — {best['grade']}")
+print(f"Eng past baho: {worst['name']} — {worst['grade']}")
+print(f"O'rtacha baho: {average}")
