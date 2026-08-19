0c01e549b9ba829ef00242f6a80288d475089239
IbrohimjonCoder <muhammadjonovichibrohimjongmail.com>
2026-08-19 15:06:23 +0500

    rearrange by frequency

--- Mustaqil/misol3.py
+++ Mustaqil/misol3.py
@@ -1,23 +1,8 @@
+from collections import Counter
+
 def rearrange_by_frequency(nums: list[int]) -> list[int]:
-    count_dict = {}
-    for num in nums:
-        if num in count_dict:
-            count_dict[num] += 1
-        else:
-            count_dict[num] = 1
+    count = Counter(nums)
 
-    unique_nums = list(set(nums))
-
-    unique_nums.sort()
-    
-    unique_nums.sort(key=lambda x: count_dict[x], reverse=True)
-    
-
-    result = []
-    for num in unique_nums:
-        sanoq = count_dict[num]
-        result.extend([num] * sanoq)
-
-    return result
+    return sorted(nums, key=lambda x: (-count[x], x))
 
 print(rearrange_by_frequency([4, 5, 6, 5, 4, 3, 4]))
